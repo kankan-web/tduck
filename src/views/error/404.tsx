834213c1155189bc3ee30a94bcc95c7b65af8e2b
kankan-web <2728360489@qq.com>
2024-03-24 20:38:05 +0800

    build: 404页面实现

--- src/views/error/404.tsx
+++ src/views/error/404.tsx
@@ -1,11 +1,20 @@
 import { FC } from "react";
-
+import { Result, Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import { MANAGE_INDEX_PATHNAME } from "../../routers";
 const NotFound: FC = () => {
+	const nav = useNavigate();
 	return (
-		<div className="not-found">
-			<h1>404</h1>
-			<p>Page not found</p>
-		</div>
+		<Result
+			status="404"
+			title="404"
+			subTitle="抱歉，您访问的页面不存在"
+			extra={
+				<Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>
+					返回首页
+				</Button>
+			}
+		/>
 	);
 };
 export default NotFound;
