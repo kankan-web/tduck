f33aaad4cb3dd67b8e4890a7dc48f6d644cccde9
kankan-web <2728360489@qq.com>
2024-04-13 21:53:20 +0800

    feat: 组件分组显示在组件库中

--- src/views/question/Edit/LeftPanel/ComponentLib/ComponentLib.tsx
+++ src/views/question/Edit/LeftPanel/ComponentLib/ComponentLib.tsx
@@ -1,13 +1,27 @@
 import { FC } from "react";
 import { Typography } from "antd";
-import { componentConfigGroup } from "../../../../../components/QuestionComponent";
+import {
+	componentConfigGroup,
+	ComponentConfigType
+} from "../../../../../components/QuestionComponent";
+import styles from "./ComponentLib.module.scss";
 
 const { Title } = Typography;
+function getComponent(c: ComponentConfigType) {
+	const { title, type, Component } = c;
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.component}>
+				<Component />
+			</div>
+		</div>
+	);
+}
 const Lib: FC = () => {
 	return (
 		<>
 			{componentConfigGroup.map((group, index) => {
-				const { groupId, groupName } = group;
+				const { groupId, groupName, components } = group;
 				return (
 					<div key={groupId}>
 						<Title
@@ -15,6 +29,7 @@ const Lib: FC = () => {
 							style={{ fontSize: "16px", marginTop: index > 0 ? "16px" : "0" }}
 						>
 							{groupName}
+							<div>{components.map(c => getComponent(c))}</div>
 						</Title>
 					</div>
 				);
