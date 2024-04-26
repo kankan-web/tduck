db036ec1b3ac92173a2d3f1f3dbc20cb54302e82
kankan-web <2728360489@qq.com>
2024-04-26 23:17:05 +0800

    feat: 问卷测试配置搭建并进行QuestionInfo组件单元测试

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+/// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	test: {
+		globals: true,
+		environment: "jsdom",
+		// environment: "happy-dom"
+		setupFiles: ["./src/setup.ts"]
+	},
 	plugins: [react()],
 	server: {
 		port: 8000,
